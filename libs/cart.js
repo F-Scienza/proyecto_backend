b3cf24677eb7fe5b34afadb363325a00e4eef2a2
F-Scienza <cufa1905@gmail.com>
2022-02-25 03:47:40 -0300

    todos los metodos terminados

--- libs/cart.js
+++ libs/cart.js
@@ -4,48 +4,48 @@ class Cart {
 	constructor(fileName) {
 		this.file = fileName;
 		this.countID = 0;
-		this.carts = []
+		this.carts = [];
 	}
 	async init() {
-		console.log('-----carts init-----')
+		console.log('-----carts init-----');
 		const data = await fs.promises.readFile(this.file);
-		const dataCarts = JSON.parse(data); 
-		for(const el of dataCarts){
-			this.carts.push(el)
-			this.countID++
+		const dataCarts = JSON.parse(data);
+		for (const el of dataCarts) {
+			this.carts.push(el);
+			this.countID++;
 		}
-		console.log('-----cart file loaded----')
+		console.log('-----cart file loaded----');
 	}
 	async write() {
-		console.log('-----carts write-----')
-		await fs.promises.writeFile(this.file, JSON.stringify(this.carts))
+		console.log('-----carts write-----');
+		await fs.promises.writeFile(this.file, JSON.stringify(this.carts));
 	}
-	async newCart(obj){
-		console.log('-----new cart-----')
-		this.countID++
-		obj['id']=this.countID
-		this.carts.push(obj)
-		this.write()
-		return console.log(`new cart whit id ${this.countID}`)
+	async newCart(obj) {
+		console.log('-----new cart-----');
+		this.countID++;
+		obj['id'] = this.countID;
+		this.carts.push(obj);
+		this.write();
+		return console.log(`new cart whit id ${this.countID}`);
 	}
-	async deleteCart(id){
-		console.log('-----delete cart-----')
-		if(this.carts.length > 0){
+	async deleteCart(id) {
+		console.log('-----delete cart-----');
+		if (this.carts.length > 0) {
 			console.log(JSON.stringify(this.carts) + ' antes de filtrar ');
-			let newList = this.carts.filter((x)=> x.id != id)
-			this.carts = newList
-			this.write()
-			console.log(`-----cart ${id} deleted-----`);	
-		}else{
-			console.log('-----no carts-----')
+			let newList = this.carts.filter(x => x.id != id);
+			this.carts = newList;
+			this.write();
+			console.log(`-----cart ${id} deleted-----`);
+		} else {
+			console.log('-----no carts-----');
 		}
 	}
-	async getCartProducts(id){
-		console.log('-----search cart-----')
-		let selectedCart = this.carts.find(el => el.id == id)
-		return selectedCart
+	async getCartProducts(id) {
+		console.log('-----search cart-----');
+		let selectedCart = this.carts.find(el => el.id == id);
+		return selectedCart;
 	}
-	async addProduct(id, product){
+	async addProduct(id, product) {
 		const index = this.carts.findIndex(objT => objT.id == id);
 		if (this.carts[index]) {
 			this.carts[index].products.push(product);
@@ -53,7 +53,20 @@ class Cart {
 		} else {
 			console.log('no existe carrito con ese id');
 		}
-		return this.carts
+		return this.carts;
+	}
+	async deleteProductCart(id_cart, id_prod) {
+		if(this.carts !== []){
+			const index = this.carts.findIndex(obj => obj.id == id_cart);
+			const cartProducts = this.carts[index].products
+			const newCartProducts = cartProducts.filter(x => x.id !== id_prod);
+			this.carts[index].products = newCartProducts
+			console.log('-----Deleted product on cart-----');
+			this.write();
+		}else{
+			console.log('-----no exist-----');
+		}
+		return 
 	}
 }
 module.exports = Cart;
--- routers/cart.router.js
+++ routers/cart.router.js
@@ -22,6 +22,12 @@ routerCart.delete('/:id', (req, res)=>{
     let content = carts.carts
     return res.json(content)
 })
+routerCart.get('/', (req, res) => {
+	//  lista de todos los carritos
+    console.log('-----get all carts-----');
+    let content = carts.carts;
+	return res.json(content);
+});
 routerCart.get('/:id/products', (req, res)=>{
     //  lista de productos en el carrito
     let id = Number(req.params.id)
@@ -31,13 +37,17 @@ routerCart.get('/:id/products', (req, res)=>{
 routerCart.post('/:id/products', (req, res)=>{
     //  agrega productos al carrito por id de producto
     let id = Number(req.params.id)
-    let product = req.body 
+    let product = req.body
     carts.addProduct(id, product)
     let content = carts.carts
     return res.json(content)
 })
-routerCart.delete('/:id/productos/:id_prod', (req, res)=>{
+routerCart.delete('/:id/products/:id_prod', (req, res)=>{
     //  elimina un producto del carrito
+    let id_prod = Number(req.params.id_prod)
+    let id_cart = Number(req.params.id)
+    carts.deleteProductCart(id_cart, id_prod);
+    return res.json(carts.carts)
 })
 
 module.exports = routerCart;
